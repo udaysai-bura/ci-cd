de9c76dda27b255484b6664c7d756e44f4ba2f01
uday <udaysai.bura@zemosolabs.com>
2026-03-12 17:35:55 +0530

    break test

--- test_app.py
+++ test_app.py
@@ -1,4 +1,17 @@
-from app import add
+from app import add, subtract
 
-def test_add():
+
+def test_add_positive():
     assert add(2,3) == 5
+
+
+def test_add_negative():
+    assert add(-1,-1) == -2
+
+
+def test_subtract():
+    assert subtract(5,3) == 2
+
+
+def test_add_zero():
+    assert add(0,0) == 0
